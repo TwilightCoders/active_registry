66ab7fc11085fe1774787f58ec016676b522c179
Jason Poppler <popp0102@gmail.com>
2018-03-20 14:19:21 -0600

    find will iterate over all indexes values pairs, intersect the subsets and create a new registry based on that subset of indexes. added specs. registry now stores sets instead of arrays. created custom matchers on specs for registry mapping expectations

---
+++ spec/support/custom_matchers.rb
@@ -0,0 +1,18 @@
+RSpec::Matchers.define :contain_mappings do |expected_mappings|
+
+  match do |registry|
+    expected_mappings.each do |index_name, expected_mapping|
+      expected_mapping.each do |index, expected|
+        actual = registry.find(index_name => index).to_a
+
+        actual.sort!{ |a, b| a.object_id <=> b.object_id }
+        expected.sort!{ |a, b| a.object_id <=> b.object_id }
+
+        return false unless actual == expected
+      end
+    end
+
+    true
+  end
+end
+
