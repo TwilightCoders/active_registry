cf45455f5fe962b3c0d6465074c060aeec3f8402
jpoppler <jason@getnotion.com>
2018-03-20 10:11:51 -0600

    place tests in contexts

--- spec/registry/registry_spec.rb
+++ spec/registry/registry_spec.rb
@@ -2,93 +2,86 @@ require 'spec_helper'
 
 RSpec.describe Registry do
   Person = Struct.new(:id, :name, :email)
-  let(:u1) {
-    Person.new(1, 'Dale', 'dale@twilightcoders.net')
-  }
+  let(:u1) { Person.new(1, 'Dale', 'dale@twilightcoders.net') }
+  let(:u2) { Person.new(2, 'Dale', 'dale@chillywinds.com') }
+  let(:u3) { Person.new(3, 'Foo', 'foobar@twilightcoders.net') }
 
-  let(:u2) {
-    Person.new(2, 'Dale', 'dale@chillywinds.com')
-  }
+  context 'Adding' do
+    let(:r1) { Registry.new([ u1, u2 ]) }
 
-  let(:u3) {
-    Person.new(3, 'Foo', 'foobar@twilightcoders.net')
-  }
+    it 'should add the correct item' do
+      r1 << u3
 
-  let(:r1) {
-    Registry.new([
-      u1,
-      u2
-    ])
-  }
-
-  let(:r2) {
-    Registry.new([
-      u1,
-      u2
-    ])
-  }
-
-  it 'adds & deletes' do
-    r1 << u3
-
-    expect(r1.to_h).to eq({
-      object_id: {
-        u1.object_id => [u1],
-        u2.object_id => [u2],
-        u3.object_id => [u3]
-      }
-    })
-
-    r1.delete(u2)
-
-    expect(r1.to_h).to eq({
-      object_id: {
-        u1.object_id => [u1],
-        u3.object_id => [u3]
-      }
-    })
+      expect(r1.to_h).to eq({
+        object_id: {
+          u1.object_id => [u1],
+          u2.object_id => [u2],
+          u3.object_id => [u3]
+        }
+      })
+    end
   end
 
-  it 'indexes' do
-    r2.index(:name)
+  context 'Deleting' do
+    let(:r1) { Registry.new([ u1, u2 ]) }
 
-    expect(r2.to_h).to eq({
-      object_id: {
-        u1.object_id => [u1],
-        u2.object_id => [u2]
-      },
-      name: {
-        "Dale" => [
-          u1, u2
-        ]
-      }
-    })
+    before(:each) do
+      r1 << u3
+    end
 
+    it 'should remove the correct item' do
+      r1.delete(u2)
 
+      expect(r1.to_h).to eq({
+        object_id: {
+          u1.object_id => [u1],
+          u3.object_id => [u3]
+        }
+      })
+    end
   end
 
-  it 'reindexes' do
-    r2.index(:name)
+  context "Indexing" do
+    let(:r2) { Registry.new([ u1, u2 ]) }
 
-    d = r2[:name, 'Dale'].first
-    d.name = "Bob"
+    it 'indexes' do
+      r2.index(:name)
 
-    expect(r2.first(:name, 'Bob')).to eq(d)
+      expect(r2.to_h).to eq({
+        object_id: {
+          u1.object_id => [u1],
+          u2.object_id => [u2]
+        },
+        name: {
+          "Dale" => [
+            u1, u2
+          ]
+        }
+      })
+    end
+
+    it 'reindexes' do
+      r2.index(:name)
+
+      d = r2[:name, 'Dale'].first
+      d.name = "Bob"
+
+      expect(r2[:name, 'Bob'].first).to eq(d)
+    end
+
+    it 'unwatches' do
+      r2.index(:name)
+
+      d = r2[:name, 'Dale'].first
+
+      expect(d.methods).to include(:__watched_name=, :__unwatched_name=)
+
+      r2.delete(d)
+
+      expect(d.methods).to_not include(:__watched_name=, :__unwatched_name=)
+
+      d.name="Bob"
+      expect(d.name).to eq("Bob")
+    end
   end
-
-  it 'unwatches' do
-    r2.index(:name)
-
-    d = r2[:name, 'Dale'].first
-
-    expect(d.methods).to include(:__watched_name=, :__unwatched_name=)
-
-    r2.delete(d)
-
-    expect(d.methods).to_not include(:__watched_name=, :__unwatched_name=)
-
-    d.name="Bob"
-    expect(d.name).to eq("Bob")
-  end
-
 end
